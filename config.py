e9b353b73d369321ca92b2b25d288495694154cb
rahulboss905 <166490010+rahulboss905@users.noreply.github.com>
2025-08-11 10:23:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID","21803165"))
 API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","7767108161"))
+OWNER_ID = int(getenv("OWNER_ID","7456681709"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 MUST_JOIN = getenv("team_secrat_bots","save_restricted_contenttt")
